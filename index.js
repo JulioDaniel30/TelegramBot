a31ee03ae8c1b32a30bbe69d490e8a6b0bb943ba
JulioDaniel30 <82523805+JulioDaniel30@users.noreply.github.com>
2022-08-19 21:12:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const puppeteer = require('puppeteer');
 
 async function getAcoes(page,acao) {
     await page.goto(`https://www.google.com/finance/quote/${acao}:BVMF`);
-    const valor = await page.evaluate(() => {
-      return document.querySelector('.kf1m0 .YMlKec').innerText;
+    const valor = await page.evaluate( async () => {
+      return await document.querySelector('.kf1m0 .YMlKec').innerText;
     })
     const percentual = await page.evaluate(() => {
       return document.querySelector('.enJeMd').innerText;
@@ -36,9 +36,14 @@ bot.on('message',  async(msg) => {
     }
 
     if(await msg.text === '/goodactions'){
-        bot.sendMessage(chatId, "Carregando...")
-        const browser = await puppeteer.launch();
+        await bot.sendMessage(chatId, "Carregando...")
+        const browser = await puppeteer.launch({
+          headless:true,
+          args:['--no-sandbox']
+        });
+        await console.log("Browser aberto")
         const page = await browser.newPage();
+        await console.log("Página aberta")
 
         let acoes = {
             PETR3 : await getAcoes(page,'PETR3'),
@@ -47,7 +52,6 @@ bot.on('message',  async(msg) => {
             VALE3 : await getAcoes(page,'VALE3'),
             BBDC4 : await getAcoes(page,'BBDC4'),
             CPFE3 : await getAcoes(page,'CPFE3'),
-            CPFE6 : await getAcoes(page,'CPFE6'),
             ITUB4 : await getAcoes(page,'ITUB4'),
             GOAU4 : await getAcoes(page,"GOAU4"), 
             MRFG3 : await getAcoes(page,"MRFG3"),
@@ -57,6 +61,7 @@ bot.on('message',  async(msg) => {
             BRAP4 : await getAcoes(page,"BRAP4")
 
         };
+        await console.log("Ações carregadas")
 
         await page.close();
         await browser.close();
@@ -69,7 +74,6 @@ MGLU3: ${acoes.MGLU3.valor} | (${acoes.MGLU3.percentual})
 VALE3: ${acoes.VALE3.valor} | (${acoes.VALE3.percentual})
 BBDC4: ${acoes.BBDC4.valor} | (${acoes.BBDC4.percentual})
 CPFE3: ${acoes.CPFE3.valor} | (${acoes.CPFE3.percentual})
-CPFE3: ${acoes.CPFE6.valor} | (${acoes.CPFE6.percentual})
 ITUB4: ${acoes.ITUB4.valor} | (${acoes.ITUB4.percentual})
 GOAU4: ${acoes.GOAU4.valor} | (${acoes.GOAU4.percentual})
 MRFG3: ${acoes.MRFG3.valor} | (${acoes.MRFG3.percentual})
@@ -79,9 +83,9 @@ CMIG4: ${acoes.CMIG4.valor} | (${acoes.CMIG4.percentual})
 BRAP4: ${acoes.BRAP4.valor} | (${acoes.BRAP4.percentual})
 
         `)
-
+        await console.log("Resposta gerada")
         await bot.sendMessage(chatId,resposta)
-        
+        await console.log("Mensagem enviada")
     }
 });
 
